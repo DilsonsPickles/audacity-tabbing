bffdef58fcf27ee53fc0d763d4de379d363e97f7
Dilson's Pickles <a.dawson@mu.se>
2025-04-08 13:37:32 +1000

    Remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import ProjectToolbar from "@/components/Toolbars/ProjectToolbar";
 import MainToolbar from "@/components/Toolbars/MainToolbar";
 import TrackHeadersPanel from "@/components/Panels/TrackHeadersPanel";
